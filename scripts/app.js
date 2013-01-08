10dce533d3727a7189e3a0414f6fcb6a029a6352
Nick Mathis <nick@simreg.net>
2013-01-08 15:18:47 -0500

    Page 6

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 $(function() {
-   App.currentUser = new App.Models.User();
-   var loginView = new App.Views.Login({el: $('#loginView'), model: App.currentUser});
-   var newQuestionView = new App.Views.NewQuestion({el: $('#newQuestionView')});
+   App.currentUser = new App.Models.User();
+   var questions = new App.Collections.Questions();
+ 
+   var loginView = new App.Views.Login({el: $('#loginView'), model: App.currentUser});
+   var newQuestionView = new App.Views.NewQuestion({el: $('#newQuestionView'), model: questions});
+   var questionsView = new App.Views.Questions({el: $('#questions'), model: questions});
 
 });
--- scripts/views/new_question.js
+++ scripts/views/new_question.js
@@ -1,5 +1,8 @@
 App.Views.NewQuestion = Backbone.View.extend({
- 
+   events: {
+       "click #addItem": "addItem"
+   },
+  
    initialize: function(options) {
        App.currentUser.on("change", this.render, this);
        this.render();
@@ -9,6 +12,16 @@ App.Views.NewQuestion = Backbone.View.extend({
        this.$("#asker").text(App.currentUser.get("userName"));
        this.$el.toggleClass("hidden", !App.currentUser.isLoggedIn());
        return this;
-   }
+   },
+
+  addItem: function() {
+        var question = {
+            text: this.$("#newQuestion").val(),
+            userName: App.currentUser.get("userName")
+        };
+
+        this.model.add(new App.Models.Question(question));
+        this.$("#newQuestion").val('')
+    },
  
 });
