9b5ed20d52103ae4d3354bc690276eb1c94b23e1
Nick Mathis <nick@simreg.net>
2013-01-08 14:53:47 -0500

    Page 4

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,6 @@
 $(function() {
    App.currentUser = new App.Models.User();
    var loginView = new App.Views.Login({el: $('#loginView'), model: App.currentUser});
+   var newQuestionView = new App.Views.NewQuestion({el: $('#newQuestionView')});
+
 });
